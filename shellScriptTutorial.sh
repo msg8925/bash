147745a170ca7b478df7e03fa3e735800ac4ca11
unknownAuthor <msg.8925@gmail.com>
2022-01-06 03:29:35 -0500

    bash script to echo out variables.

---
+++ shellScriptTutorial.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+
+# echo out "Hello world".
+echo hello world.
+
+# variables
+FIRSTNAME="Mike"
+LASTNAME="Peterson"
+
+# echo out variable 'FIRSTNAME'
+echo "My firstname is $FIRSTNAME."
+
+# echo out variable 'LASTNAME' (alternate syntax)
+echo "My lastname is ${LASTNAME}"
