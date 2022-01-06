f7f48d875938cbfbde52c73cf50db67b6568dd04
unknownAuthor <msg.8925@gmail.com>
2022-01-06 04:52:01 -0500

    Created a file conditional to check whether a file called 'text.txt' exists

--- shellScriptTutorial.sh
+++ shellScriptTutorial.sh
@@ -2,15 +2,16 @@
 
 
 # echo out "Hello world".
-echo hello world.
+#echo hello world.
 
 # variables
 #FIRSTNAME="Mike"
 #LASTNAME="Peterson"
 #COMPUTER_ANSWER="Anna"
 #COMPUTER_ANSWER_TWO="Jack"
-NUM1=""
-NUM2=""
+#NUM1=""
+#NUM2=""
+FILE="text.txt"
 
 # echo out variable 'FIRSTNAME'
 #echo "My firstname is $FIRSTNAME."
@@ -19,10 +20,10 @@ NUM2=""
 #echo "My lastname is ${LASTNAME}"
 
 # get input from command line
-read -p "Please enter first number: " NUM1
+#read -p "Please enter first number: " NUM1
 
 # print user input 
-read -p "Please enter second number: " NUM2
+#read -p "Please enter second number: " NUM2
 
 # simple if else condition
 #if [ "$NAME" == "$COMPUTER_ANSWER" ]
@@ -35,9 +36,25 @@ read -p "Please enter second number: " NUM2
 #	echo "I guessed your name incorrectly. My answer: $COMPUTER_ANSWER. "
 #fi
 
-if [ "$NUM1" -gt "$NUM2" ]
+#if [ "$NUM1" -gt "$NUM2" ]
+#then
+#	echo "$NUM1 is greater than $NUM2"
+#else	
+#	echo "$NUM2 is greater than $NUM1"
+#fi
+
+#Create a file called "text.txt" and place in the same directory as this .exe
+if [ -e "$FILE" ]
 then
-	echo "$NUM1 is greater than $NUM2"
-else	
-	echo "$NUM2 is greater than $NUM1"
+	echo "$FILE exists."
+else
+	echo "$FILE does not exist."
 fi	
+
+if [ -f "$FILE" ]
+then
+	echo "$FILE is a file."
+else
+	echo "$FILE is not a file."		
+fi
+
