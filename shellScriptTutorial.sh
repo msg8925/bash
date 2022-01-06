42a17385a548b07d58c3cf553cfb47cdb88a9b17
unknownAuthor <msg.8925@gmail.com>
2022-01-06 04:13:56 -0500

    Added an else to the if conditional statement

--- shellScriptTutorial.sh
+++ shellScriptTutorial.sh
@@ -7,6 +7,7 @@ echo hello world.
 # variables
 FIRSTNAME="Mike"
 LASTNAME="Peterson"
+COMPUTER_ANSWER="Anna"
 
 # echo out variable 'FIRSTNAME'
 echo "My firstname is $FIRSTNAME."
@@ -18,10 +19,12 @@ echo "My lastname is ${LASTNAME}"
 read -p "Please enter your name: " NAME
 
 # print user input 
-echo "Your name is $NAME."
+#echo "Your name is $NAME."
 
-# simple if condition
-if [ "$NAME" == "Anna" ]
+# simple if else condition
+if [ "$NAME" == "$COMPUTER_ANSWER" ]
 then 
-	echo "I guessed your name correctly $NAME"
+	echo "I guessed your name correctly. My answers: $COMPUTER_ANSWER."
+else   
+	echo "I guessed your name incorrectly. My answer: $COMPUTER_ANSWER. "
 fi       	
